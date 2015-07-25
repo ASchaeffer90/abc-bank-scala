76231173e925a16e3750bc53db931c1ae613a60f
ASchaeffer15 <andytheman2990@aol.com>
2015-07-25 13:41:42 -0400

    Make test description more clear.  Add test for interest on more then one accounts

--- src/test/scala/com/abc/BankTest.scala
+++ src/test/scala/com/abc/BankTest.scala
@@ -4,14 +4,14 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class BankTest extends FlatSpec with Matchers {
 
-  "Bank" should "customer summary" in {
+  "Bank Manager" should "be able to request a customer summary" in {
     val bank: Bank = new Bank
-    var john: Customer = new Customer("John").openAccount(new Account(Account.CHECKING))
+    val john: Customer = new Customer("John").openAccount(new Account(Account.CHECKING))
     bank.addCustomer(john)
     bank.customerSummary should be("Customer Summary\n - John (1 account)")
   }
 
-  it should "checking account" in {
+  it should "be able to check the interest paid on a checking account" in {
     val bank: Bank = new Bank
     val checkingAccount: Account = new Account(Account.CHECKING)
     val bill: Customer = new Customer("Bill").openAccount(checkingAccount)
@@ -20,7 +20,7 @@ class BankTest extends FlatSpec with Matchers {
     bank.totalInterestPaid should be(0.1)
   }
 
-  it should "savings account" in {
+  it should "be able to check the interest paid on a savings account" in {
     val bank: Bank = new Bank
     val checkingAccount: Account = new Account(Account.SAVINGS)
     bank.addCustomer(new Customer("Bill").openAccount(checkingAccount))
@@ -28,12 +28,25 @@ class BankTest extends FlatSpec with Matchers {
     bank.totalInterestPaid should be(2.0)
   }
 
-  it should "maxi savings account" in {
+  it should "be able to check the interest paid on a maxi savings account" in {
     val bank: Bank = new Bank
     val checkingAccount: Account = new Account(Account.MAXI_SAVINGS)
     bank.addCustomer(new Customer("Bill").openAccount(checkingAccount))
     checkingAccount.deposit(3000.0)
     bank.totalInterestPaid should be(170.0)
+  }
+
+  it should "be able to check the total interest paid on multiple different opend accounts" in {
+    val bank: Bank = new Bank
+    val maxiSavingsAccount: Account = new Account(Account.MAXI_SAVINGS)
+    val checkingAccount: Account = new Account(Account.CHECKING)
+    val customerBill: Customer = new Customer("Bill")
+    customerBill.openAccount(maxiSavingsAccount)
+    customerBill.openAccount(checkingAccount)
+    bank.addCustomer(customerBill)
+    checkingAccount.deposit(100.0)
+    maxiSavingsAccount.deposit(3000.0)
+    bank.totalInterestPaid should be(170.1)
   }
 
 }
